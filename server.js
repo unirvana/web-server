52fd179f79fe2eab7d6ddf6f96ca5cd0b287b964
unirvana69 <unirvana@gmail.com>
2016-06-03 15:09:09 +0200

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res) {
-  res.send('About page');
+  res.send('About page!');
 });
 
 app.use(express.static(__dirname + '/public'));
