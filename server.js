e3dbdabe9117d77b8c42fca3f72b342980a6f33e
unirvana69 <unirvana@gmail.com>
2016-06-03 15:06:37 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+  requireAuthentication: function(req, res, next) {
+    console.log('private route hit!');
+    next();
+  },
+  logger: function(req, res, next) {
+    console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+    next();
+  }
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+  res.send('About page');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+  console.log('Express server started and listening on port ' + PORT + '!');
+});
